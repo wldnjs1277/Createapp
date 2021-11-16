93f46f05769a9a494bc34e9c1ee3c3c9616a734b
wldnjs1277@naver.com <42260892+wldnjs1277@users.noreply.github.com>
2021-11-17 00:48:59 +0900

    -navcontroller-

--- app/src/main/java/com/example/lovetest/MainActivity.kt
+++ app/src/main/java/com/example/lovetest/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.lovetest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var navcontroller: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navcontroller = nav_host_fragment.findNavController()
+
     }
 }
