fa3eec530469d73686503f667279ac024b2a218e
wldnjs1277@naver.com <42260892+wldnjs1277@users.noreply.github.com>
2021-11-19 14:19:26 +0900

    -result fragment-

--- app/src/main/java/com/example/lovetest/fragment/ResultFragment.kt
+++ app/src/main/java/com/example/lovetest/fragment/ResultFragment.kt
@@ -1,11 +1,15 @@
 package com.example.lovetest.fragment
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.example.lovetest.R
+import kotlinx.android.synthetic.main.fragment_result.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -29,13 +33,49 @@ class ResultFragment : Fragment() {
             param2 = it.getString(ARG_PARAM2)
         }
     }
+    var option = -1
+    lateinit var navController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+        option = arguments?.getInt("index") ?:-1
+
         return inflater.inflate(R.layout.fragment_result, container, false)
+
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        navController=Navigation.findNavController(view)
+
+        setResult(option){}
+    }
+    @SuppressLint("SetTextI18n")
+    fun setResult(option:Int){
+        when(option){
+            1 -> {
+                result_main.text = "YOU are a You are a QUITTER!"
+                result_sub.text = "You can let the person easily."
+
+            }
+            2 -> {
+                result_main.text="You should focus on yourself"
+                result_sub.text="You become really clingy to your ex."
+            }
+            3 -> {
+                result_main.text="You should take it easy"
+                result_sub.text="You can do crazy things no matter what it takes."
+            }
+            4 -> {
+                result_main.text="You are pretty mature."
+                result_sub.text="You can easily accept the break-up."
+            }
+        }
     }
 
     companion object {
